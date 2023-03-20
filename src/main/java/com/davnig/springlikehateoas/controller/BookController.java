b92188fc67bc9fad0d0cc7c7e9b29cd951ef310c
davnig <daddets97@gmail.com>
2023-03-20 17:42:22 +0100

    refactor: rename controller mapping

--- src/main/java/com/davnig/springlikehateoas/controller/BookController.java
+++ src/main/java/com/davnig/springlikehateoas/controller/BookController.java
@@ -12,14 +12,14 @@ import static com.davnig.springlikehateoas.utils.DummyInvocationUtils.methodOn;
 
 
 @RestController
-@RequestMapping("/greetings")
+@RequestMapping("/books")
 public class BookController {
 
     @GetMapping("/1")
-    public ResponseEntity<Book> greeting() {
+    public ResponseEntity<Book> demo() {
         Book book = new Book(1, "Harry Potter")
-                .add(Link.of("http://localhost:8080/greetings/1"))
-                .add(Link.to(methodOn(BookController.class).greeting()));
+                .add(Link.of("http://localhost:8080/books/1"))
+                .add(Link.to(methodOn(BookController.class).demo()));
         return ResponseEntity.ok(book);
     }
 
